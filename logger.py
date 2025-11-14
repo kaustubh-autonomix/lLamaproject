ee1257243d23ec51801c711d76ca1ce54b8fdd70
kaustubh <kaustubh@autonomixsolutions.com>
2025-11-14 21:01:41 +0530

    added a line

--- logger.py
+++ logger.py
@@ -3,6 +3,7 @@ import logging
 logger = logging.getLogger("lLamaproject")
 logger.setLevel(logging.INFO)
 
+
 handler = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
