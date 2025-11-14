4ded736c3ded975472867553d5afd5968ef8c2ac
kaustubh <kaustubh@autonomixsolutions.com>
2025-11-14 21:18:11 +0530

    useless file deleted

--- logger.py
+++ logger.py
@@ -3,7 +3,6 @@ import logging
 logger = logging.getLogger("lLamaproject")
 logger.setLevel(logging.INFO)
 
-
 handler = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from lLamaproject!")
-
-
-if __name__ == "__main__":
-    main()
